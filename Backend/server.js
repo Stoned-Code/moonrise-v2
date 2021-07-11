bc87479c755a3468a1fdd4bb2f41f6f1e74eb2a1
Stoned-Code <animecomcommunity@gmail.com>
2021-07-11 05:19:44 -0700

    .

--- Backend/server.js
+++ Backend/server.js
@@ -212,9 +212,9 @@ app.post('/' + adduser, function(req, res)
 {
     let user = req.body;
 
-    user['DisplayName'] = Buffer.from(user['DisplayName'], 'base64');
-    user['UserId'] = Buffer.from(user['UserId'], 'base64');
-    user['MoonriseKey'] = Buffer.from(user['MoonriseKey'], 'base64');
+    user['DisplayName'] = Buffer.from(user['DisplayName'], 'base64').toString();
+    user['UserId'] = Buffer.from(user['UserId'], 'base64').toString();
+    user['MoonriseKey'] = Buffer.from(user['MoonriseKey'], 'base64').toString();
     
     console.log(JSON.stringify(user));
     // moonrisedb.insert(user);
